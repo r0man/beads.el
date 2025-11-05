ef1e0e74bb78b9d1b619a823d5ad3e90d6f07d5a
Roman Scherer <roman@burningswell.com>
2025-11-05 22:17:28 +0100

    Add share/emacs.d/init.el

---
+++ share/emacs.d/init.el
@@ -0,0 +1,15 @@
+(use-package ef-themes
+  :hook (after-init . (lambda() (ef-themes-rotate ef-themes-collection))))
+
+(use-package magit
+  :bind (("C-x C-g s" . magit-status)))
+
+(use-package beads
+  :commands (beads)
+  :load-path ("~/workspace/beads.el/lisp"
+              "~/workspace/beads.el/lisp/test")
+  :bind ("C-c b" . beads)
+  :hook (after-init . (lambda ()
+                        (require 'beads-eldoc)
+                        (beads-eldoc-mode)
+                        (beads))))
