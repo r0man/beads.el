69f64fc0e0597c1eb1ffbc6d606792926dced27d
Roman Scherer <roman@burningswell.com>
2025-11-07 12:35:22 +0100

    Use vertico for testing

--- share/emacs.d/init.el
+++ share/emacs.d/init.el
@@ -1,7 +1,7 @@
 ;;; init.el --- Minimal init.el for testing  -*- lexical-binding: t; -*-
 
 (use-package ef-themes
-  :hook (after-init . (lambda() (ef-themes-rotate ef-themes-collection))))
+  :hook (after-init . (lambda () (ef-themes-rotate ef-themes-collection))))
 
 (use-package magit
   :bind (("C-x C-g s" . magit-status)))
@@ -11,7 +11,12 @@
   :load-path ("~/workspace/beads.el/lisp"
               "~/workspace/beads.el/lisp/test")
   :bind ("C-c b" . beads)
-  :hook (after-init . (lambda ()
-                        (require 'beads-eldoc)
-                        (beads-eldoc-mode)
-                        (beads))))
+  :hook (after-init . beads)
+  :config
+  (require 'beads-eldoc)
+  (beads-eldoc-mode))
+
+(use-package vertico
+  :hook (after-init . vertico-mode)
+  :custom
+  (vertico-cycle t))
