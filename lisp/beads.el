d253f50e976e3a07dd28c3481b905d4d5f1e4233
Roman Scherer <roman@burningswell.com>
2025-11-30 11:19:17 +0000

    Require transient 0.11.0 (latest) and Emacs 28.1
    Update package dependencies to require:
    - transient 0.11.0 (latest from GNU ELPA, Nov 2025)
    - Emacs 28.1 (required by transient 0.11.0)
    
    This ensures we get the version with transient-define-group
    and all the latest improvements.

--- lisp/beads.el
+++ lisp/beads.el
@@ -4,7 +4,7 @@
 
 ;; Author: Beads Contributors
 ;; Version: 0.1.0
-;; Package-Requires: ((emacs "27.1") (transient "0.10.1"))
+;; Package-Requires: ((emacs "28.1") (transient "0.11.0"))
 ;; Keywords: tools, project, issues
 ;; URL: https://github.com/josephburnett/beads
 
