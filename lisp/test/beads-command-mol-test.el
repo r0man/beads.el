eb038cab9a6db529c24f02bd04fe3668cc339d3b
furiosa <roman@burningswell.com>
2026-03-24 00:50:37 +0100

    feat: add bd mol wisp subcommands (wisp-create, wisp-list, wisp-gc) (be-nkz)
    Add three new EIEIO command classes to beads-command-mol.el matching
    the upstream wispCreateCmd, wispListCmd, and wispGCCmd subcommands:
    
    - beads-command-mol-wisp-create: bd mol wisp create <proto-id>
      with --dry-run, --root-only, --var options
    - beads-command-mol-wisp-list: bd mol wisp list
      with --all, --type options
    - beads-command-mol-wisp-gc: bd mol wisp gc
      with --dry-run, --age, --all, --closed, --force, --exclude-type options
    
    Includes transient menu definitions and 13 new unit tests.

--- lisp/test/beads-command-mol-test.el
+++ lisp/test/beads-command-mol-test.el
@@ -50,6 +50,110 @@
     (should (member "wisp" args))
     (should (member "proto-1" args))))
 
+;;; Unit Tests: beads-command-mol-wisp-create command-line
+
+(ert-deftest beads-command-mol-wisp-create-test-command-line-basic ()
+  "Unit test: mol wisp create builds correct command line."
+  :tags '(:unit)
+  (let* ((cmd (beads-command-mol-wisp-create :proto-id "proto-1"))
+         (args (beads-command-line cmd)))
+    (should (member "mol" args))
+    (should (member "wisp" args))
+    (should (member "create" args))
+    (should (member "proto-1" args))))
+
+(ert-deftest beads-command-mol-wisp-create-test-command-line-dry-run ()
+  "Unit test: mol wisp create includes --dry-run flag."
+  :tags '(:unit)
+  (let* ((cmd (beads-command-mol-wisp-create :proto-id "proto-1"
+                                             :dry-run t))
+         (args (beads-command-line cmd)))
+    (should (member "--dry-run" args))))
+
+(ert-deftest beads-command-mol-wisp-create-test-command-line-root-only ()
+  "Unit test: mol wisp create includes --root-only flag."
+  :tags '(:unit)
+  (let* ((cmd (beads-command-mol-wisp-create :proto-id "proto-1"
+                                             :root-only t))
+         (args (beads-command-line cmd)))
+    (should (member "--root-only" args))))
+
+(ert-deftest beads-command-mol-wisp-create-test-command-line-var ()
+  "Unit test: mol wisp create includes --var option."
+  :tags '(:unit)
+  (let* ((cmd (beads-command-mol-wisp-create :proto-id "proto-1"
+                                             :var '("key=value")))
+         (args (beads-command-line cmd)))
+    (should (member "--var" args))
+    (should (member "key=value" args))))
+
+;;; Unit Tests: beads-command-mol-wisp-list command-line
+
+(ert-deftest beads-command-mol-wisp-list-test-command-line-basic ()
+  "Unit test: mol wisp list builds correct command line."
+  :tags '(:unit)
+  (let* ((cmd (beads-command-mol-wisp-list))
+         (args (beads-command-line cmd)))
+    (should (member "mol" args))
+    (should (member "wisp" args))
+    (should (member "list" args))))
+
+(ert-deftest beads-command-mol-wisp-list-test-command-line-all ()
+  "Unit test: mol wisp list includes --all flag."
+  :tags '(:unit)
+  (let* ((cmd (beads-command-mol-wisp-list :show-all t))
+         (args (beads-command-line cmd)))
+    (should (member "--all" args))))
+
+(ert-deftest beads-command-mol-wisp-list-test-command-line-type ()
+  "Unit test: mol wisp list includes --type option."
+  :tags '(:unit)
+  (let* ((cmd (beads-command-mol-wisp-list :type-filter "agent"))
+         (args (beads-command-line cmd)))
+    (should (member "--type" args))
+    (should (member "agent" args))))
+
+;;; Unit Tests: beads-command-mol-wisp-gc command-line
+
+(ert-deftest beads-command-mol-wisp-gc-test-command-line-basic ()
+  "Unit test: mol wisp gc builds correct command line."
+  :tags '(:unit)
+  (let* ((cmd (beads-command-mol-wisp-gc))
+         (args (beads-command-line cmd)))
+    (should (member "mol" args))
+    (should (member "wisp" args))
+    (should (member "gc" args))))
+
+(ert-deftest beads-command-mol-wisp-gc-test-command-line-dry-run ()
+  "Unit test: mol wisp gc includes --dry-run flag."
+  :tags '(:unit)
+  (let* ((cmd (beads-command-mol-wisp-gc :dry-run t))
+         (args (beads-command-line cmd)))
+    (should (member "--dry-run" args))))
+
+(ert-deftest beads-command-mol-wisp-gc-test-command-line-force ()
+  "Unit test: mol wisp gc includes --force flag."
+  :tags '(:unit)
+  (let* ((cmd (beads-command-mol-wisp-gc :force t))
+         (args (beads-command-line cmd)))
+    (should (member "--force" args))))
+
+(ert-deftest beads-command-mol-wisp-gc-test-command-line-age ()
+  "Unit test: mol wisp gc includes --age option."
+  :tags '(:unit)
+  (let* ((cmd (beads-command-mol-wisp-gc :age "30m"))
+         (args (beads-command-line cmd)))
+    (should (member "--age" args))
+    (should (member "30m" args))))
+
+(ert-deftest beads-command-mol-wisp-gc-test-command-line-exclude-type ()
+  "Unit test: mol wisp gc includes --exclude-type option."
+  :tags '(:unit)
+  (let* ((cmd (beads-command-mol-wisp-gc :exclude-type '("agent")))
+         (args (beads-command-line cmd)))
+    (should (member "--exclude-type" args))
+    (should (member "agent" args))))
+
 ;;; Unit Tests: beads-command-mol-bond command-line
 
 (ert-deftest beads-command-mol-bond-test-command-line-basic ()
